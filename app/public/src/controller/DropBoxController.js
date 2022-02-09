0c2bbbcdd41bd2509a6bb0696e55e6ee520489f2
Everson <eversonpereiraofc@gmail.com>
2022-02-09 14:58:40 -0300

    enviando arquivos com AJAX e recebendo no servidor node com Formidable

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -12,7 +12,38 @@ class DropBoxController{
         })
 
         this.inputFilesEl.addEventListener("change", e=>{
+            this.uploadTask(e.target.files)
             this.snackModalEl.style.display = "block"
         })
+    }
+
+    uploadTask(files){
+        let promises= [];
+        [...files].forEach(file=>{
+            promises.push(new Promise((resolve, reject) => {
+                let ajax = new XMLHttpRequest()
+                ajax.open("POST", "/upload")
+
+                ajax.onerror = e=>{
+                    reject(e)
+                }
+
+                ajax.onload = file=>{
+                    try{
+                        resolve(JSON.parse(ajax.responseText))
+                    } catch (e){
+                        reject(e)
+                    }
+                }
+
+                let formData = new FormData()
+                formData.append("input-file", file)
+                ajax.send(formData)
+            }))
+        })
+
+        console.log(files)
+
+        return Promise.all(promises)
     }
 }
